7e006d7e4f9fa2785ea5e76c0030e9e4c4fce12e
Carpio, Beejay A. <56281375+Suppelong@users.noreply.github.com>
2026-01-27 09:09:20 +0800

    Add view and insert comment/visitor feedback functionality to your website

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import Profile from './components/Profile.vue' 
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
+import CommentForm from './components/CommentForm.vue'
+import Comment from './components/Comment.vue'
 
 const app = createApp(App)
 
@@ -10,5 +12,6 @@ const app = createApp(App)
 app.component('profile', Profile)
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
-
+app.component('comment-form', CommentForm)
+app.component('comment', Comment)
 app.mount('#app')
